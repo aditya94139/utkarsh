906cc9ce1e437aaae75fb3cdf6c8f41a5f165a17
Lord <110597375+kd2929@users.noreply.github.com>
2023-10-21 21:15:08 +0530

    Update sam (2).py

--- sam (2).py
+++ sam (2).py
@@ -1,55 +1,35 @@
-#  MIT License
-#
-#  Copyright (c) 2019-present Dan <https://github.com/delivrance>
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE
-#  Code edited By Cryptostark
-import urllib
-import urllib.parse
-import requests
-import json
+import requests, json, zipfile, io, re, os
 import subprocess
-from pyrogram.types.messages_and_media import message
 import helper
 from pyromod import listen
 from pyrogram.types import Message
 import tgcrypto
 import pyrogram
 from pyrogram import Client, filters
-from pyrogram.types.messages_and_media import message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import FloodWait
 import time
 from pyrogram.types import User, Message
 from p_bar import progress_bar
 from subprocess import getstatusoutput
 import logging
-import os
-import sys
-import re
-from pyrogram import Client as bot
-import cloudscraper
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
-from base64 import b64encode, b64decode
+#from jinja2 import Template
+# from details import api_id, api_hash, bot_token
+from urllib.parse import unquote
 
-@bot.on_message(filters.command(["samyak"]) & ~filters.edited)
+# import requests
+# bot = Client(
+#     "bot",
+#     api_id=api_id,
+#     api_hash=api_hash,
+#     bot_token=bot_token)
+
+
+bot = Client(
+    "Utkarsh",
+    bot_token=os.environ.get("BOT_TOKEN"),
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH")
+)
+@bot.on_message(filters.command(["samyak"]))
 async def account(bot: Client, m: Message):
     global cancel
     s = requests.Session()
